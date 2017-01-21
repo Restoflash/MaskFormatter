e0e18485575789ec52aea27a6eddda1d19073976
dvoiss <davidvoiss@gmail.com>
2017-01-21 00:23:13 -0800

    Support dash characters.

--- maskformatter/src/main/java/com/azimolabs/maskformatter/MaskFormatter.java
+++ maskformatter/src/main/java/com/azimolabs/maskformatter/MaskFormatter.java
@@ -11,9 +11,9 @@ public class MaskFormatter implements TextWatcher {
 
     private static final char SPACE = ' ';
 
-    private String mask;
-
-    private EditText maskedField;
+    private final String mask;
+    private final EditText maskedField;
+    private final char maskCharacter;
 
     private boolean editTextChange;
     private int newIndex;
@@ -21,8 +21,13 @@ public class MaskFormatter implements TextWatcher {
     private int selectionBefore;
 
     public MaskFormatter(String mask, EditText maskedField) {
+        this(mask, maskedField, SPACE);
+    }
+
+    public MaskFormatter(String mask, EditText maskedField, char maskCharacter) {
         this.mask = mask;
         this.maskedField = maskedField;
+        this.maskCharacter = maskCharacter;
     }
 
     @Override
@@ -77,7 +82,7 @@ public class MaskFormatter implements TextWatcher {
         }
 
         if (selectionBefore - 1 >= 0
-            && appliedMaskString.charAt(selectionBefore - 1) == SPACE) {
+            && appliedMaskString.charAt(selectionBefore - 1) == maskCharacter) {
             return selectionBefore - 1;
         }
 
@@ -89,7 +94,7 @@ public class MaskFormatter implements TextWatcher {
             return appliedMaskString.length();
         }
 
-        if (appliedMaskString.charAt(selectionBefore) == SPACE) {
+        if (appliedMaskString.charAt(selectionBefore) == maskCharacter) {
             return selectionBefore + 2;
         }
 
@@ -105,15 +110,15 @@ public class MaskFormatter implements TextWatcher {
     }
 
     private String applyMask(String newValue) throws InvalidTextException {
-        String newValueWithoutSpaces = newValue.replaceAll("\\s", "");
+        String newValueWithoutSpaces = newValue.replaceAll(String.valueOf(maskCharacter), "");
         StringBuilder sb = new StringBuilder();
         int index = 0;
         for (char c : newValueWithoutSpaces.toCharArray()) {
             if (index >= mask.length()) {
                 throw new InvalidTextException();
             }
-            while (mask.charAt(index) == SPACE) {
-                sb.append(SPACE);
+            while (mask.charAt(index) == maskCharacter) {
+                sb.append(maskCharacter);
                 index++;
             }
             sb.append(applyMaskToChar(c, index));
@@ -134,7 +139,7 @@ public class MaskFormatter implements TextWatcher {
         }
 
         char maskChar = getFirstNotWhiteCharFromMask();
-        if (maskChar == SPACE) {
+        if (maskChar == maskCharacter) {
             return;
         }
 
@@ -143,14 +148,13 @@ public class MaskFormatter implements TextWatcher {
 
     private char getFirstNotWhiteCharFromMask() {
         int maskIndex = maskedField.getSelectionEnd();
-        while (maskIndex < mask.length() && mask.charAt(maskIndex) == SPACE) {
+        while (maskIndex < mask.length() && mask.charAt(maskIndex) == maskCharacter) {
             maskIndex++;
         }
         return mask.charAt(maskIndex);
     }
 
     public String getRawTextValue() {
-        return maskedField.getText().toString().replaceAll("\\s", "");
+        return maskedField.getText().toString().replaceAll(String.valueOf(maskCharacter), "");
     }
-
 }
